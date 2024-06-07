b343003b7e0d0e7d881b8afb57a3f6aab7cf1a80
Ashton Carter <110428463+Ashton-Carter@users.noreply.github.com>
2024-06-07 15:22:09 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Build the Docker image
-docker build -t my-streamlit-app .
-
-# Run the Docker container
-docker run -d -p 8501:8501 my-streamlit-app
-
-# Generate a simple HTML file that redirects to the Streamlit app
-echo "<meta http-equiv=\"refresh\" content=\"0; url=http://localhost:8501/\">" > public/index.html
